5a87ec0344e6024608c90a301da8e4a4e41825d9
yyyy1999 <42821007@qq.com>
2017-04-22 11:48:45 +0200

    seconde commit

--- core/replay_buffer.py
+++ core/replay_buffer.py
@@ -264,8 +264,6 @@ class replay_buffer(object):
         #sorted(self.sorted_buffer, key=lambda s:s[1].reward, reverse=True)
         self.sorted_buffer.sort(reverse=True) 
         print self.sorted_buffer[0]
-        print self.sorted_buffer[0]
-        print self.distribution 
-         
-        print self.dist_sur_k=[] 
+        print self.distribution  
+        print self.dist_sur_k 
 
